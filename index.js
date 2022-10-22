635ea7f69433753bcab6a3d1cb80f968db394857
Mac <ashok@thebrewapps.com>
2022-10-22 17:29:41 +0530

    re deploy

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res)=>{
-    res.send("Hello Cyclic");
+    res.send("Hello Cyclic 2");
 });
 
 app.listen(port, ()=>{
